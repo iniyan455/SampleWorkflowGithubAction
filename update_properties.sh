4065eb00143452edbf4a696c6a54d4b9ee92e3f8
Iniyan Arul <iniyan.m@krazybee.com>
2021-05-07 16:10:53 +0530

    Dev test Impl

--- update_properties.sh
+++ update_properties.sh
@@ -4,5 +4,8 @@
 touch local.properties
 echo "baseUrl=\"$1\"" >> local.properties
 echo "apiKey=\"$2\"" >> local.properties
-echo "gateway=$3" >> local.properties
-echo "appType=$4" >> local.properties
+echo "gateway=\"$3\"" >> local.properties
+echo "appType=\"$4\"" >> local.properties
+
+#echo "gateway=$3" >> local.properties
+#echo "appType=$4" >> local.properties
