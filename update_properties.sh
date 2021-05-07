a684335ac514e0bd86c4ba0b85930386bb6945b3
Iniyan Arul <iniyan.m@krazybee.com>
2021-05-07 16:05:55 +0530

    Dev test Impl

--- update_properties.sh
+++ update_properties.sh
@@ -2,7 +2,7 @@
 
 # Assign the filename
 touch local.properties
-echo "BASE_URL=\"$1\"" >> local.properties
-echo "MAP_KEY=\"$2\"" >> local.properties
-echo "RETRY_ATTEMPTS=$3" >> local.properties
-echo "THRESHOLD_VALUE=$4" >> local.properties
+echo "baseUrl=\"$1\"" >> local.properties
+echo "apiKey=\"$2\"" >> local.properties
+echo "gateway=$3" >> local.properties
+echo "appType=$4" >> local.properties
